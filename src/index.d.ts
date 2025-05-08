b9e024c557a8065b5cc65a11974f86ec827b1a3c
babycommando <joao@trackers.cx>
2025-05-07 22:38:38 -0300

    added patch for typescript types

---
+++ src/index.d.ts
@@ -0,0 +1,34 @@
+// src/index.d.ts
+
+declare module "@babycommando/entity-db" {
+  export class EntityDB {
+    constructor(config: { vectorPath: string; model?: string });
+
+    insert(data: { [key: string]: any }): Promise<number>;
+    insertBinary(data: { [key: string]: any }): Promise<number>;
+    insertManualVectors(data: { [key: string]: any }): Promise<number>;
+
+    update(key: number, data: { [key: string]: any }): Promise<void>;
+    delete(key: number): Promise<void>;
+
+    query(
+      queryText: string,
+      options?: { limit?: number }
+    ): Promise<{ [key: string]: any }[]>;
+
+    queryBinary(
+      queryText: string,
+      options?: { limit?: number }
+    ): Promise<{ [key: string]: any }[]>;
+
+    queryBinarySIMD(
+      queryText: string,
+      options?: { limit?: number }
+    ): Promise<{ [key: string]: any }[]>;
+
+    queryManualVectors(
+      queryVector: number[],
+      options?: { limit?: number }
+    ): Promise<{ [key: string]: any }[]>;
+  }
+}
